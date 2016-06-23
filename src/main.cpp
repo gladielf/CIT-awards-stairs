68e17bd310a8a1a53e0347dc88cb7d1be2833f39
Fermín Conejo Baltasar <gladielf@gmail.com>
2016-06-23 18:48:34 +0200

    Migrado el proyecto desde ArduinoIDE a PlatformIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
+#include <Arduino.h>
+
 int led[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int ledLenght = sizeof(led)/sizeof(int);
 int i = 0;
 int delaySetup = 200;
 
 void setup() {
-  for(i = 0; i < 8 ; i++) {
-    pinMode(led[i], OUTPUT);  
+  Serial.begin(9600);
+  for(i = 0; i < ledLenght ; i++) {
+    pinMode(led[i], OUTPUT);
   }
 }
 
 void loop() {
-  for(i = 0; i < 8 ; i++) {
+  for(i = 0; i < ledLenght ; i++) {
     digitalWrite(led[i], HIGH);
     delay(delaySetup);
     digitalWrite(led[i], LOW);
