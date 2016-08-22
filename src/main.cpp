7644f1d77dede9054392a99371edc0ecc6c0e3f0
Fermín Conejo Baltasar <gladielf@gmail.com>
2016-08-22 22:20:11 +0200

    Añadidos los códigos en decimal de todos los botones del mando

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <IRremote.h>
+#include <citLib.h>
 
 void IRDecoder();
 
@@ -50,6 +51,8 @@ void loop() {
 void IRDecoder() {
   if (irrecv.decode(&results)) {
     switch (results.value) {
+      //FILA 1
+      /*** 1ª Fila con botones encencido ***/
       // mas: FF3AC5 >>> 16726725
       case 16726725: // Más velocidad (se REDUCE el delay)
           if (delaySetup > 0) {
@@ -62,6 +65,163 @@ void IRDecoder() {
           delaySetup += delayChangeValue;
           flag = 1;
           break;
+      // Play?: FF827D >>> 16745085
+      case 16745085: //
+          break;
+      // On/Off: FF02FD >>> 16712445
+      case 16712445: //
+          break;
+
+      //FILA 2
+      /*** 1ª Fila de colores con texto RGBW ***/
+      // RED R: FF1AE5 >>> 16718565
+      case 16718565: //
+          break;
+      // GREEN G: FF9A65 >>> 16751205
+      case 16751205: //
+          break;
+      // BLUE B: FFA25D >>> 16753245
+      case 16753245: //
+          break;
+      // WHITE W: FF22DD >>> 16720605
+      case 16720605: //
+          break;
+
+      //FILA 3
+      /*** 2ª Fila de colores ***/
+      // Naranjita: FF2AD5 >>> 16722645
+      case 16722645: //
+          break;
+      // Verdecillo: FFAA55 >>> 16755285
+      case 16755285: //
+          break;
+      // azulillo: FF926D >>> 16749165
+      case 16749165: //
+          break;
+      // blaco roto?: FF12ED >>> 16716525
+      case 16716525: //
+          break;
+
+      //FILA 4
+      /*** 3ª Fila de colores ***/
+      // Amarillo huevo: FF0AF5 >>> 16714485
+      case 16714485: //
+          break;
+      // Azul: FF8A75 >>> 16747125
+      case 16747125: //
+          break;
+      // Morado: FFB24D >>> 16757325
+      case 16757325: //
+          break;
+      // color carne: FF32CD >>> 16724685
+      case 16724685: //
+          break;
+
+      //FILA 5
+      /*** 4ª Fila de colores ***/
+      // Marron: FF38C7 >>> 16726215
+      case 16726215: //
+          break;
+      // verde azulado: FFB847 >>> 16758855
+      case 16758855: //
+          break;
+      // rosita: FF7887 >>> 16742535
+      case 16742535: //
+          break;
+      // azul claro: FFF807 >>> 16775175
+      case 16775175: //
+          break;
+
+      //FILA 6
+      /*** 5ª Fila de colores ***/
+      // Amarillo: FF18E7 >>> 16718055
+      case 16718055: //
+          break;
+      // azul: FF9867 >>> 16750695
+      case 16750695: //
+          break;
+      // magenta: FF58A7 >>> 16734375
+      case 16734375: //
+          break;
+      // azul claro: FFD827 >>> 16767015
+      case 16767015: //
+          break;
+
+      //FILA 7
+      /*** 1ª Fila de flechas ***/
+      // UP RED: FF28D7 >>> 16722135
+      case 16722135: //
+          break;
+      // UP GREEN: FFA857 >>> 16754775
+      case 16754775: //
+          break;
+      // UP BLUE: FF6897 >>> 16738455
+      case 16738455: //
+          break;
+      // Quick: FFE817 >>> 16771095
+      case 16771095: //
+          break;
+
+      //FILA 8
+      /*** 2ª Fila de flechas ***/
+      // DOWN RED: FF08F7 >>> 16713975
+      case 16713975: //
+          break;
+      // DOWN GREEN: FF8877 >>> 16746615
+      case 16746615: //
+          break;
+      // DOWN BLUE: FF48B7 >>> 16730295
+      case 16730295: //
+          break;
+      // Slow: FFC837 >>> 16762935
+      case 16762935: //
+          break;
+
+      //FILA 9
+      /*** 1ª Fila de DIY ***/
+      // DIY1: FF30CF >>> 16724175
+      case 16724175: //
+          break;
+      // DIY2: FFB04F >>> 16756815
+      case 16756815: //
+          break;
+      // DIY3: FF708F >>> 16740495
+      case 16740495: //
+          break;
+      // auto: FFF00F >>> 16773135
+      case 16773135: //
+          break;
+
+      //FILA 10
+      /*** 2ª Fila de DIY ***/
+      // DIY4: FF10EF >>> 16716015
+      case 16716015: //
+          break;
+      // DIY5: FF906F >>> 16748655
+      case 16748655: //
+          break;
+      // DIY6: FF50AF >>> 16732335
+      case 16732335: //
+          break;
+      // FLASH: FFD02F >>> 16764975
+      case 16764975: //
+          break;
+
+      //FILA 11
+      /*** Fila de Jumps y Fades ***/
+      // JUMP3: FF20DF >>> 16720095
+      case 16720095: //
+          break;
+      // JUMP7: FFA05F >>> 16752735
+      case 16752735: //
+          break;
+      // FADE3: FF609F >>> 16736415
+      case 16736415: //
+          break;
+      // FADE7: FFE01F >>> 16769055
+      case 16769055: //
+          break;
+
       default:
           // de momento no hago nada si recibe un dato diferente a los que yo quiero
           break;
